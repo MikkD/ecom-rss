480c67ed7dceeb30509399bfc7a61d86c6cf4f71
Mikita <ndosyukov@gmail.com>
2023-08-02 19:15:29 -0700

    migrated-assets-for-express-response-handler

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
-import sliderAssets from '../../data/sliderAssets.json';
+import React, { useState, useEffect } from 'react';
+import fallBackSliderAssets from '../../data/sliderAssets.json'; //TODO-REMOVE-AFTER-MIGRATION
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 import './Slider.scss';
 
 function Slider() {
     const [transitionStep, setTransitionStep] = useState(0);
+    const [sliderAssets, setSliderAssets] = useState([]);
+    const [isError, setIsError] = useState(false);
     const translateXStep = 100 / sliderAssets.length; //25%
     const translateXStepLimit = 100 - translateXStep;
+    const shouldShowSliderContorls = sliderAssets.length > 1;
+
+    const getSliderAssets = async () => {
+        try {
+            const assets = await fetch('http://localhost:5000/slider-assets');
+            const parsedAssets = await assets.json();
+            setSliderAssets(parsedAssets);
+        } catch (e) {
+            console.error(e);
+            setIsError(true);
+        }
+    };
+
+    useEffect(() => {
+        getSliderAssets();
+    }, []);
 
     const handlePrevSlider = () =>
         setTransitionStep((prevStep) => {
@@ -20,26 +38,33 @@ function Slider() {
             if (prevStep === translateXStepLimit) return 0;
             return prevStep + translateXStep;
         });
+
     return (
         <div className='slider'>
-            <div
-                style={{
-                    transform: `translateX(-${transitionStep}%)`,
-                    width: `(${sliderAssets.length}vw)`,
-                }}
-                className='slider-imgs-wrapper'>
-                {sliderAssets.map(({ id, imgUrl, name }) => (
-                    <img className='slider-img' key={id} src={imgUrl} alt={name} />
-                ))}
-            </div>
-            <div className='slider-controls'>
-                <button onClick={handlePrevSlider}>
-                    <ArrowLeftIcon />
-                </button>
-                <button onClick={handleNextSlider}>
-                    <ArrowRightIcon />
-                </button>
-            </div>
+            {isError ? (
+                <h4>Error...</h4>
+            ) : (
+                <div
+                    style={{
+                        transform: `translateX(-${transitionStep}%)`,
+                        width: `(${sliderAssets.length}vw)`,
+                    }}
+                    className='slider-imgs-wrapper'>
+                    {sliderAssets.map(({ id, imgUrl, name }) => (
+                        <img className='slider-img' key={id} src={imgUrl} alt={name} />
+                    ))}
+                </div>
+            )}
+            {shouldShowSliderContorls && (
+                <div className='slider-controls'>
+                    <button onClick={handlePrevSlider}>
+                        <ArrowLeftIcon />
+                    </button>
+                    <button onClick={handleNextSlider}>
+                        <ArrowRightIcon />
+                    </button>
+                </div>
+            )}
         </div>
     );
 }
--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-//server
+const express = require('express');
+const cors = require('cors');
+const sliderAssets = require('./data/sliderAssets.json');
+
+const app = express();
+app.use(cors());
+
+app.get('/slider-assets', (req, res) => {
+    res.json(sliderAssets);
+});
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
